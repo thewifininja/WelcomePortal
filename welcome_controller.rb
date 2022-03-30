acfe39130d81982af92ae8e7957c935dde9180f9
Nelson Karrick <nkarrick@stepcg.com>
2022-03-30 15:17:16 -0400

    Initial Push

---
+++ welcome_controller.rb
@@ -0,0 +1,11 @@
+class Portal::WelcomeController < PortalController
+
+# WARNING: modifying this file is not recommended unless you are familiar with
+# developing for the Ruby on Rails web application framework!
+#
+# This class inherits the default portal's action methods from PortalController.
+# Methods defined here will be available as custom actions of this custom portal
+# controller.
+
+end
+
